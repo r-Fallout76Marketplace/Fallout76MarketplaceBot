2a35e965f38ff0de3b1170261eaee34d6b7e7052
isFakeAccount <trevorphillips@gmx.us>
2020-09-27 13:42:28 -0400

    Added try catch to make sure bot doesn't go down during error 503

--- fallout76marketplace_bot.py
+++ fallout76marketplace_bot.py
@@ -40,33 +40,32 @@ def refresh_memory():
 
 
 schedule.every(5).hours.do(refresh_memory)
-
 print("The bot has started running...")
 
-while True:
-    schedule.run_pending()
-    # Gets comments and if it receives None, it switches to posts
-    for comment in comment_stream:
-        if comment is None:
-            break
-        try:
-            marketplace_database.load_comment(comment, database)
-        except Exception:
-            # Sends a message to mods in case of error
-            tb = traceback.format_exc()
-            CONFIG.reddit.redditor("is_fake_Account").message(CONFIG.subreddit_name, tb,
-                                                              from_subreddit=CONFIG.subreddit_name)
-            print(tb)
+# Boolean variable to make sure error message is sent only once
+message_sent = False
 
-    # Gets posts and if it receives None, it switches to comments
-    for submission in submission_stream:
-        if submission is None:
-            break
-        try:
+while True:
+    message_sent = False
+    # Try catch to make sure bot doesn't go down during Error 503
+    try:
+        schedule.run_pending()
+        # Gets comments and if it receives None, it switches to posts
+        for comment in comment_stream:
+            if comment is None:
+                break
+            marketplace_database.load_comment(comment, database)
+
+        # Gets posts and if it receives None, it switches to comments
+        for submission in submission_stream:
+            if submission is None:
+                break
             marketplace_database.load_submission(submission)
-        except Exception:
-            # Sends a message to mods in case of error
-            tb = traceback.format_exc()
-            CONFIG.reddit.redditor("is_fake_Account").message(CONFIG.subreddit_name, tb,
-                                                              from_subreddit=CONFIG.subreddit_name)
-            print(tb)
+    except Exception:
+        # Sends a message to mods in case of error
+		if not message_sent:
+			tb = traceback.format_exc()
+			CONFIG.reddit.redditor("is_fake_Account").message(CONFIG.subreddit_name, tb,
+															  from_subreddit=CONFIG.subreddit_name)
+			print(tb)
+			message_sent = True
